6ab31c784cf292c3cd7beb40456f48f1949f69b7
hengolivia <53484921+oliviaaheng@users.noreply.github.com>
2026-01-31 16:31:32 -0500

    adding in more items for output schema

--- backend/search.py
+++ backend/search.py
@@ -20,6 +20,7 @@ def get_travel_json(user_input: dict):
     # Use types.GenerateContentConfig for strict parameter naming
     config = types.GenerateContentConfig(
         response_mime_type="application/json",
+
         response_schema={
             "type": "ARRAY",
             "items": {
@@ -27,16 +28,20 @@ def get_travel_json(user_input: dict):
                 "properties": {
                     "name": {"type": "STRING"},
                     "description": {"type": "STRING"},
-                    "address": {"type": "STRING"}
+                    "address": {"type": "STRING"},
+                    "website": {"type": "STRING"},
+                    "image": {"type": "STRING"},
+                    "cost": {"type": "STRING"},
+                    "category": {"type": "STRING"}
                 },
-                "required": ["name", "description", "address"]
+                "required": ["name", "description", "address","website", "cost", "category"]
             }
         }
     )
 
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents=f"Generate 15 travel recommendations for {user_input['destination']} on {user_input['date_of_travel']}.",
+        contents=f"Generate 14 travel recommendations given the constraints of {user_input['destination']} on {user_input['date_of_travel']}. Keep in mind the following: {user_input['freeform_text']}.",
         config=config  # Use 'config', not 'generation_config'
     )
     
@@ -46,7 +51,8 @@ def get_travel_json(user_input: dict):
 input_data = {
     "destination": "Porto, Portugal",
     "date_of_travel": "May 9, 2026",
-    "address": "130 Waterman St. Providence, RI 02906"
+    "address": "130 Waterman St. Providence, RI 02906",
+    "freeform_text": "i am a relaxed traveler who likes starting my day around 10 am"
 }
 
 if __name__ == "__main__":
